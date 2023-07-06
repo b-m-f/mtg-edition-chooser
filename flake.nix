292fa125f8810faaef9a89399fce52d34b7b3252
Maximilian Ehlers <maximilian@ehlers.berlin>
2023-07-06 15:27:30 +0200

    Updates flake to work with playwright
    Signed-off-by: Maximilian Ehlers <maximilian@ehlers.berlin>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix flake for mtg edition chooser";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/23de9f3b56e72632c628d92b71c47032e14a3d4d";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -22,11 +22,13 @@
     in
     {
       devShells.default = pkgs.mkShell {
-        packages = with pkgs; [  pkgs.nodejs-18_x pkgs.playwright-driver ];
+        packages = with pkgs; [  pkgs.nodejs-18_x pkgs.playwright-test ];
 
         PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}";
 
         shellHook = ''
+          # Remove playwright from node_modules, so it will be taken from playwright-test
+          rm node_modules/@playwright/ -R
           echo "Ready to blog"
         '';
       };
